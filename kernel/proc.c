8c0440c45f049d06ed85a963784d8a283b0bdfcb
HoangBach <adamhermes66@gmail.com>
2026-01-28 14:54:05 +0700

    Add systrace and wc

--- kernel/proc.c
+++ kernel/proc.c
@@ -279,6 +279,9 @@ kfork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // COPY TRACE MASK
+  np->tracemask = p->tracemask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
--- user/wc.c
+++ user/wc.c
@@ -5,51 +5,89 @@
 
 char buf[512];
 
+// option flags (global so wc() can see them)
+int show_l = 0, show_w = 0, show_c = 0;
+
 void
 wc(int fd, char *name)
 {
   int i, n;
-  int l, w, c, inword;
+  int l = 0, w = 0, c = 0;
+  int inword = 0;
 
-  l = w = c = 0;
-  inword = 0;
-  while((n = read(fd, buf, sizeof(buf))) > 0){
-    for(i=0; i<n; i++){
+  while ((n = read(fd, buf, sizeof(buf))) > 0) {
+    for (i = 0; i < n; i++) {
       c++;
-      if(buf[i] == '\n')
+      if (buf[i] == '\n')
         l++;
-      if(strchr(" \r\t\n\v", buf[i]))
+      if (strchr(" \r\t\n\v", buf[i]))
         inword = 0;
-      else if(!inword){
+      else if (!inword) {
         w++;
         inword = 1;
       }
     }
   }
-  if(n < 0){
+
+  if (n < 0) {
     printf("wc: read error\n");
     exit(1);
   }
-  printf("%d %d %d %s\n", l, w, c, name);
+
+  if (show_l)
+    printf("%d ", l);
+  if (show_w)
+    printf("%d ", w);
+  if (show_c)
+    printf("%d ", c);
+
+  if (name[0] != 0)
+    printf("%s", name);
+
+  printf("\n");
 }
 
 int
 main(int argc, char *argv[])
 {
-  int fd, i;
+  int fd;
+  int i = 1;
 
-  if(argc <= 1){
+  // parse options
+  while (i < argc && argv[i][0] == '-') {
+    if (strcmp(argv[i], "-l") == 0)
+      show_l = 1;
+    else if (strcmp(argv[i], "-w") == 0)
+      show_w = 1;
+    else if (strcmp(argv[i], "-c") == 0)
+      show_c = 1;
+    else {
+      printf("wc: unknown option %s\n", argv[i]);
+      continue;
+    }
+    i++;
+  }
+
+  // default: show all
+  if (!show_l && !show_w && !show_c) {
+    show_l = show_w = show_c = 1;
+  }
+
+  // stdin case
+  if (i == argc) {
     wc(0, "");
     exit(0);
   }
 
-  for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], O_RDONLY)) < 0){
+  // files
+  for (; i < argc; i++) {
+    if ((fd = open(argv[i], O_RDONLY)) < 0) {
       printf("wc: cannot open %s\n", argv[i]);
-      exit(1);
+      continue;
     }
     wc(fd, argv[i]);
     close(fd);
   }
+
   exit(0);
 }
